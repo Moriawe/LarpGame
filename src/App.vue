19fa6dee4c6dd8acd749afed2045b7d79ca005cb
Moriawe <91130763+Moriawe@users.noreply.github.com>
2022-07-14 09:26:47 +0200

    rough base

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script>
-  import CitiesList from './components/CitiesList.vue'
-  import HelloWorld from './components/HelloWorld.vue'
+import GameView from "./views/GameView.vue"
 
-  export default {
-    components: {
-      CitiesList,
-      HelloWorld
-    }
-  }
+export default {
+  components: {
+    GameView,
+  },
+}
 </script>
 
 <template>
-  <HelloWorld msg="Hello World!" />
-  <CitiesList />
+  <GameView />
 </template>
